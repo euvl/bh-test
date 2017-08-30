6755becc155b7ab9bc21a5cf7ef52fb3fec79534
euvl <yev.vlasenko@gmail.com>
2017-08-30 08:28:37 +0100

    Added more tests

--- src/Checkbox/Checkbox.test.js
+++ src/Checkbox/Checkbox.test.js
@@ -7,4 +7,28 @@ describe('Checkbox component', () => {
     let component = shallow(<Checkbox/>)
     expect(component.exists()).toBe(true)
   })
+
+  it('Should have label', () => {
+    let component = shallow(<Checkbox label={'foo'}/>)
+
+    expect(component.contains(<div className="checkbox-label">foo</div>))
+      .toBe(true)
+  })
+
+  it('Should have default "checked" state', () => {
+    let component = shallow(<Checkbox/>)
+    expect(component.state().checked).toBe(false)
+  })
+
+  it('Should be checkable', () => {
+    let component = shallow(<Checkbox/>)
+
+    expect(component.state().checked).toBe(false)
+    expect(component.find('.checkbox-checked').length).toBe(0)
+
+    component.find('.checkbox-core').simulate('click')
+
+    expect(component.state().checked).toBe(true)
+    expect(component.find('.checkbox-checked').length).toBe(1)
+  })
 })
